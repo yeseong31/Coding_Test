50c997b7d3c01dc8b471993f0f100aa5508c8fe6
Yeseong31 <66625672+yeseong31@users.noreply.github.com>
2021-02-24 11:55:30 +0900

    Add files via upload

---
+++ appendixB/B_01.py
@@ -0,0 +1,17 @@
+# 소수 구하기
+
+# 에라토스테네스의 체
+m, n = map(int, input().split())
+d = [True for _ in range(n + 1)]
+d[1] = False
+
+for i in range(2, int(n ** 0.5) + 1):
+    if d[i]:
+        j = 2
+        while i * j <= n:
+            d[i * j] = False
+            j += 1
+
+for i in range(m, n + 1):
+    if d[i]:
+        print(i)
